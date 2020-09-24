c5f17932fed274e0bb38937b3c89aad1669f4917
AryaVerma <33662807+AryaVerma@users.noreply.github.com>
2020-09-24 13:18:57 +0530

    Add files via upload

---
+++ prims for adj. list Set(ElogV).cpp
@@ -0,0 +1,68 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define inf 1e9
+
+
+/* Note:
+        All the vertices are considered as indexed 0
+*/
+
+
+
+void showMst(int parent[],int key[],vector<vector<pair<int,int>>>& g)
+{
+    int ans=0;
+    for(int i=1;i<g.size();i++)
+       {
+           cout<<parent[i]<<" connected to "<<i<<" with weight equal to "<<key[i]<<endl;
+           ans+=key[i];
+       }
+    cout<<"Total minimum cost of joining all the cities is "<<ans<<endl;
+}
+
+
+int main()
+{
+     int n;
+     cin>>n;
+     int e;
+     cin>>e;
+     vector<vector<pair<int,int>>> g(n);
+     for(int i=0;i<e;i++)
+     {
+         int u,v,w;
+         cin>>u>>v>>w;
+         u--;v--;
+         g[u].push_back({v,w});
+         g[v].push_back({u,w});
+     }
+     set<pair<int,int>> minKey;
+     for(int i=1;i<n;i++)
+     minKey.insert({inf,i});
+     minKey.insert({0,0});
+     int parent[n]={-1};
+     int key[n];
+     bool mstSet[n]={false};
+     for(int i=0;i<n;i++)
+        key[i]=inf;
+
+     key[0]=0;
+     parent[0]=-1;
+     for(int i=0;i<n;i++){
+        int ver=minKey.begin()->second;
+        mstSet[ver]=true;
+        for(auto neighbours:g[ver])
+        {
+            int j=neighbours.first;
+            int w=neighbours.second;
+            if(mstSet[j]==false && key[j]>w )
+            {
+                minKey.erase(minKey.find({key[j],j}));
+                minKey.insert({w,j});
+                key[j]=w;
+                parent[j]=ver;
+            }
+        }
+     }
+     showMst(parent,key,g);
+}
